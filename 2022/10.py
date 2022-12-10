3b802625fbfafcd65f9ff534ebceb59dfdbf690c
William Tisäter <william@mason.app>
2022-12-11 00:23:49 +0100

    Rename

--- 2022/10.py
+++ 2022/10.py
@@ -2,26 +2,26 @@ from helper import load_input
 
 input = load_input("10")
 
-cycles = [20, 60, 100, 140, 180, 220]
-
 
 class CRT:
     def __init__(self):
         self.display = ""
 
-    def print(self, x):
+    def draw(self, x):
         pos = len(self.display) % 40
         if pos >= x - 1 and pos <= x + 1:
             self.display += "#"
         else:
             self.display += "."
 
-    def draw(self):
+    def show_display(self):
         for i in range(0, len(self.display), 40):
             print(self.display[i : i + 40])
 
 
 class System:
+    cycles = [20, 60, 100, 140, 180, 220]
+
     def __init__(self):
         self.x = 1
         self.cycle = 0
@@ -30,11 +30,11 @@ class System:
 
     def tick(self):
         self.cycle += 1
-        if self.cycle in cycles:
+        if self.cycle in System.cycles:
             self.signal_strength += self.x * self.cycle
-        self.crt.print(self.x)
+        self.crt.draw(self.x)
 
-    def execute(self):
+    def run(self):
         for instruction in input:
             if instruction.startswith("addx"):
                 value = int(instruction.split(" ")[1])
@@ -49,13 +49,13 @@ class System:
 
 def part_one():
     system = System()
-    return system.execute()
+    return system.run()
 
 
 def part_two():
     system = System()
-    system.execute()
-    return system.crt.draw()
+    system.run()
+    return system.crt.show_display()
 
 
 print("Part one:", part_one())
